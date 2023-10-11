f959fd49a53c1a337a193cbe433eae76ab837030
vedant jangid <vedjangid341@gmail.com>
2023-10-12 01:50:34 +0530

    flight price option list complete

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import SearchForm from "./src/Components/SearchForm";
+import FlightOptionItem from "./src/Components/FlightOptionItem";
+import data from "./data.json";
 
 export default function App() {
   return (
-    <View style={{}}>
+    <View style={{ flex: "1" }}>
       <SafeAreaView>
         <SearchForm />
+
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <FlightOptionItem flight={item} />}
+          showsVerticalScrollIndicator={false}
+        />
+
         <StatusBar style="auto" />
       </SafeAreaView>
     </View>
